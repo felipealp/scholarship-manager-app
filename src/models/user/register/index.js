16da6b3e2d64ad7bdfc94a4616a90d354468964c
felipealp <felipealvesalp@gmail.com>
2023-08-20 09:59:40 -0300

    feat: campus and projects requests

--- src/models/user/register/index.js
+++ src/models/user/register/index.js
@@ -1,3 +1,25 @@
+import { get } from 'services/request';
+
+const getCampus = async () => {
+  const campus = await get('/campus');
+
+  if (campus.data) {
+    return campus.data;
+  }
+
+  return [];
+};
+
+const getProjects = async () => {
+  const projects = await get('/projetos');
+
+  if (projects.data) {
+    return projects.data;
+  }
+
+  return [];
+};
+
 const postRegister = (data) => {
   return new Promise((resolve, reject) => {
     console.log('cadastrar: ', data);
@@ -12,50 +34,4 @@ const postRegister = (data) => {
   });
 };
 
-const getCampus = () => {
-  return new Promise((resolve) => {
-    console.log('campus: ');
-    const campus = [
-      {
-        "id": 1,
-        "nome": "UFERSA - Pau dos Ferros",
-        "endereco": "BR-226, s/n, Pau dos Ferros - RN, 59900-000"
-      }
-    ]
-    // Simulate an asynchronous operation
-    setTimeout(() => {
-      // if (data.email && data.password) {
-      //   resolve(`Success: Data processed - ${data}`);
-      // } else {
-        resolve(campus);
-      // }
-    }, 1000); // Simulating a delay of 1 second
-  });
-};
-
-const getProjects = () => {
-  return new Promise((resolve) => {
-    console.log('campus: ');
-    const campus = [
-      {
-        "id": 1,
-        "nome": "Projeto I - Sistemas",
-        "descricao": "Aplicações WEB",
-        "data_inicio": "2023-08-19",
-        "data_fim": "2024-08-19",
-        "campus": 1,
-        "coordenador": null
-      }
-    ]
-    // Simulate an asynchronous operation
-    setTimeout(() => {
-      // if (data.email && data.password) {
-      //   resolve(`Success: Data processed - ${data}`);
-      // } else {
-        resolve(campus);
-      // }
-    }, 1000); // Simulating a delay of 1 second
-  });
-};
-
-export { getCampus,getProjects, postRegister };
+export { getCampus, getProjects, postRegister };
