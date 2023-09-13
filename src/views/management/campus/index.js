cd2e683f9e8423515559a95e761c90a61018b139
felipealp <felipealvesalp@gmail.com>
2023-09-13 11:50:11 -0300

    feat: campus list integration

--- src/views/management/campus/index.js
+++ src/views/management/campus/index.js
@@ -1,47 +1,48 @@
+import { useEffect, useState } from 'react';
+
 // material-ui
 import { Grid } from '@mui/material';
 import { gridSpacing } from 'store/constant';
 
 // project imports
 import List from 'components/list';
-const columns = [
-  // { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'Nome', flex: 1 },
-  { field: 'lastName', headerName: 'Endereço', flex: 1 },
-  // {
-  //   field: 'age',
-  //   headerName: 'Age',
-  //   type: 'number',
-  //   // width: 190,
-  // },
-  // {
-  //   field: 'fullName',
-  //   headerName: 'Full name',
-  //   description: 'This column has a value getter and is not sortable.',
-  //   sortable: false,
-  //   // width: 160,
-  //   valueGetter: (params) =>
-  //     `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  // },
-];
+import SkeletonEarningCard from 'components/cards/Skeleton/EarningCard';
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+// models
+import { getCampus } from 'models/campus';
 
 const Campus = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [campus, setCampus] = useState([]);
+
+  const columns = [
+    { field: 'nome', headerName: 'Nome', flex: 1 },
+    { field: 'endereco', headerName: 'Endereço', flex: 1 }
+  ];
+
+  const requestGetCampus = async () => {
+    setLoading(true);
+    const data = await getCampus();
+
+    if (data) {
+      setCampus(data);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    requestGetCampus();
+  }, []);
+
+  if (isLoading) {
+    return <SkeletonEarningCard />;
+  }
+
   return (
     <Grid container spacing={gridSpacing}>
       <Grid item xs={12}>
-        <List title="Lista de Campus" columns={columns} rows={rows} />
+        <List title="Lista de Campus" columns={columns} rows={campus} />
       </Grid>
     </Grid>
   );
