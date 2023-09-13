6d109c70165546831d24e3b9d519db127693af08
felipealp <felipealvesalp@gmail.com>
2023-09-13 12:16:20 -0300

    feat: dean management screen

---
+++ src/views/management/dean/index.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+
+// material-ui
+import { Grid } from '@mui/material';
+import { gridSpacing } from 'store/constant';
+
+// project imports
+import List from 'components/list';
+import SkeletonEarningCard from 'components/cards/Skeleton/EarningCard';
+
+// models
+import { getDeans } from 'models/dean';
+
+const Dean = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [deans, setDeans] = useState([]);
+
+  const columns = [
+    { field: 'matricula', headerName: 'Matrícula', renderCell: (params) => params.row.user.matricula, flex: 1 },
+    { field: 'name', headerName: 'Nome', renderCell: (params) => params.row.user.name, flex: 1 },
+    { field: 'email', headerName: 'Email', renderCell: (params) => params.row.user.email, flex: 1 }
+  ];
+
+  const requestGetDeans = async () => {
+    setLoading(true);
+    const data = await getDeans();
+    if (data) {
+      setDeans(data);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    requestGetDeans();
+  }, []);
+
+  if (isLoading) {
+    return <SkeletonEarningCard />;
+  }
+
+  return (
+    <Grid container spacing={gridSpacing}>
+      <Grid item xs={12}>
+        <List title="Lista de Pró-Reitores" columns={columns} rows={deans} />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Dean;
