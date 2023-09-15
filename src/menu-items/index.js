057a7f68ff4ce478a33c965303c955c5aae87ab0
Luiz Felipe <felipealvesalps@gmail.com>
2023-09-15 00:14:39 -0300

    refactor: map manu items for user types

--- src/menu-items/index.js
+++ src/menu-items/index.js
@@ -1,6 +1,18 @@
 import management from './management';
 import other from './reports';
 
+// student
+import studentManagement from './student/management';
+import studentRegisters from './student/registers';
+
+// advisor
+import advisorManagement from './advisor/management';
+import advisorRegisters from './advisor/registers';
+
+// coordinator
+import coordinatorManagement from './coordinator/management';
+import coordinatorRegisters from './coordinator/registers';
+
 // dean
 import deanManagement from './dean/management';
 import deanRegisters from './dean/registers';
@@ -9,7 +21,19 @@ const userType = localStorage.getItem('user_type');
 
 let menuItems;
 
-if (userType === 'reitor') {
+if (userType === 'Bolsista') {
+  menuItems = {
+    items: [studentManagement, studentRegisters]
+  };
+} else if (userType === 'Orientador') {
+  menuItems = {
+    items: [advisorManagement, advisorRegisters]
+  };
+} else if (userType === 'Coordenador') {
+  menuItems = {
+    items: [coordinatorManagement, coordinatorRegisters]
+  };
+} else if (userType === 'ProReitor') {
   menuItems = {
     items: [deanManagement, deanRegisters]
   };
