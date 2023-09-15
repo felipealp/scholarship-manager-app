b241a8ab35bf82fe943cbeb2ad5a9540c155eecf
Luiz Felipe <felipealvesalps@gmail.com>
2023-09-15 00:17:00 -0300

    refactor: get token for request service

--- src/services/configAPi.js
+++ src/services/configAPi.js
@@ -1,9 +1,7 @@
 const configApi = {
   url: 'http://127.0.0.1:8000',
   apiUrl: 'http://127.0.0.1:8000/api',
-  // timeout: 1000,
-  // token: localStorage.getItem('token')
-  token: '674226d9712f6a1e4036101c8ba4bd6ad85d743d'
+  token: localStorage.getItem('user_token')
 };
 
 export default configApi;
