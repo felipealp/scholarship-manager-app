b5feda650d7f47739717fcd2dfea0851eae0e356
felipealp <felipealvesalp@gmail.com>
2023-09-15 23:36:52 -0300

    refactor: auth components

--- src/components/auth/AuthCardWrapper.js
+++ src/components/auth/AuthCardWrapper.js
@@ -6,8 +6,6 @@ import { Box } from '@mui/material';
 // project import
 import MainCard from 'components/cards/MainCard';
 
-// ==============================|| AUTHENTICATION CARD WRAPPER ||============================== //
-
 const AuthCardWrapper = ({ children, ...other }) => (
   <MainCard
     sx={{
