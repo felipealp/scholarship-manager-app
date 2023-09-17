68b6921fc780fb504a5c5b32cc8aceab937c5983
felipealp <felipealvesalp@gmail.com>
2023-09-17 14:59:07 -0300

    style: dashboard view

--- src/views/management/dashboard/index.js
+++ src/views/management/dashboard/index.js
@@ -1,19 +1,53 @@
 import { useEffect, useState } from 'react';
 
 // material-ui
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
+
+// project imports
+import MainCard from 'components/cards';
+import SkeletonEarningCard from 'components/Skeleton';
 
 const Dashboard = () => {
   const [isLoading, setLoading] = useState(true);
+
   useEffect(() => {
-    setLoading(false);
+    setTimeout(() => {
+      setLoading(false);
+    }, 600);
   }, []);
 
   if (isLoading) {
-    return <>Carregando...</>;
+    return <SkeletonEarningCard />;
   }
 
-  return <Grid>Dashboard</Grid>;
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <MainCard>
+          <h1 style={{ color: '#1e88e5', textAlign: 'center' }}>Bem-vindo</h1>
+          <Typography style={{ textAlign: 'center' }} ariant="body1">
+            Aqui você tem total gerenciamento para bolsa acadêmica.
+          </Typography>{' '}
+        </MainCard>
+      </Grid>
+      {/* <Grid item xs={6}>
+        <MainCard>
+          <h1 style={{ color: 'black', textAlign: 'center' }}>Bem-vindo</h1>
+          <Typography style={{ textAlign: 'center' }} ariant="body1">
+            Aqui você tem total gerenciamento para bolsa acadêmica.
+          </Typography>{' '}
+        </MainCard>
+      </Grid>
+      <Grid item xs={6}>
+        <MainCard>
+          <h1 style={{ color: 'black', textAlign: 'center' }}>Bem-vindo</h1>
+          <Typography style={{ textAlign: 'center' }} ariant="body1">
+            Aqui você tem total gerenciamento para bolsa acadêmica.
+          </Typography>{' '}
+        </MainCard>
+      </Grid> */}
+    </Grid>
+  );
 };
 
 export default Dashboard;
